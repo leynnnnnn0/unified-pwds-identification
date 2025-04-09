d4d6d59bfc2bb195ed927f7c5c0e4a715f8aed6d
M-Juss <jusssayson@gmail.com>
2025-04-09 08:17:03 +0800

    Added Alert Dialog and seperate formOptions

---
+++ resources/js/lib/formOptions.js
@@ -0,0 +1,104 @@
+
+
+export const civilStatus = [
+    { label: "Single", value: "single" },
+    { label: "Seperated", value: "seperated" },
+    { label: "Cohabitation", value: "cohabitation" },
+    { label: "Married", value: "married" },
+    { label: "Widow", value: "widow" },
+];
+
+export const typeOfDisabilities = [
+    { label: "Deaf or Hard of Hearing", value: "deaf_or_hard_of_hearing" },
+    { label: "Intellectual Disability", value: "intellectual_disability" },
+    { label: "Learning Disability", value: "learning_disability" },
+    { label: "Mental Disability", value: "mental_disability" },
+    {
+        label: "Physical Disability(Orthopedic)",
+        value: "physical_disability",
+    },
+    { label: "Psychosocial Disability", value: "psychosocial_disability" },
+    {
+        label: "Speech and Language Impairment",
+        value: "speech_and_language_impairment",
+    },
+    { label: "Visual Disability", value: "visual_disability" },
+    { label: "Cancer (RA11215)", value: "cancer" },
+    { label: "Rare Disease (RA10747)", value: "rare_disease" },
+];
+
+export const congenitalCause = [
+    { label: "ADHD", value: "adhd" },
+    { label: "Cerebral Palsy", value: "cerebral_palsy" },
+    { label: "Down Syndrome", value: "down_syndrome" },
+];
+
+export const acquiredCause = [
+    { label: "ADHD", value: "adhd" },
+    { label: "Cerebral Palsy", value: "cerebral_palsy" },
+    { label: "Down Syndrome", value: "down_syndrome" },
+];
+
+export const educationalAttainment = [
+    { label: "None", value: "none" },
+    { label: "Kindergarten", value: "kindergarten" },
+    { label: "Elementary", value: "elementary" },
+    { label: "Junior High School", value: "junior_high_school" },
+    { label: "Senior High School", value: "senior_high_school" },
+    { label: "College", value: "college" },
+    { label: "Vocational", value: "vocational" },
+    { label: "Post-Graduate", value: "post_graduate" },
+];
+
+export const statusOfEmployment = [
+    { label: "Employed", value: "employed" },
+    { label: "Unemployed", value: "unemployed" },
+    { label: "Self-employed", value: "self_employed" },
+];
+
+export const typesOfEmployment = [
+    { label: "Permanent / Regular", value: "permanent_regular" },
+    { label: "Seasonal", value: "seasonal" },
+    { label: "Casual", value: "casual" },
+    { label: "Emergency", value: "emergency" },
+];
+
+export const occupations = [
+    { label: "Managers", value: "managers" },
+    { label: "Professionals", value: "professionals" },
+    {
+        label: "Technicians and Associate Professionals",
+        value: "technicians_and_associate_professionals",
+    },
+    {
+        label: "Clerical Support Workers",
+        value: "clerical_support_workers",
+    },
+    {
+        label: "Service and Sales Workers",
+        value: "service_and_sales_workers",
+    },
+    {
+        label: "Skilled Agricultural, Forestry and Fishery Workers",
+        value: "skilled_agricultural_forestry_and_fishery_workers",
+    },
+    {
+        label: "Craft and Related Trade Workers",
+        value: "craft_and_related_trade_workers",
+    },
+    {
+        label: "Plant and Machine Operators and Assemblers",
+        value: "plant_and_machine_operators_and_assemblers",
+    },
+    { label: "Elementary Occupations", value: "elementary_occupations" },
+    {
+        label: "Armed Forces Occupations",
+        value: "armed_forces_occupations",
+    },
+    { label: "Others, specify:", value: "others", hasInput: true },
+];
+
+export const categoryOfEmployment = [
+    { label: "Government", value: "government" },
+    { label: "Private", value: "private" },
+];
