979be5308074d97ccb08b785a9b1ca0cd860610d
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-05-05 22:36:07 +0800

    added psgc api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Rootscratch\PSGC\PSGC;
 
 class UserController extends Controller
 {
     public function index()
     {
         $users = User::paginate(10);
-
         return Inertia::render('User/Index', [
             'users' => $users
         ]);
@@ -19,12 +19,18 @@ class UserController extends Controller
 
     public function show(User $user)
     {
+
+
         return Inertia::render('User/Show', [
             'user' => $user,
         ]);
     }
     public function create()
     {
+        // Mr Simple
+        // https://github.com/jaycee0610/PSGC-API
+        $psgcApi = new PSGC();
+        $cAndM =  $psgcApi->MunicipalAndCities($MunicipalAndCities_ID = null);
         return Inertia::render('User/Create');
     }
     public function edit(User $user)
